b414e7bc09ad6764a6b12656333f2e614ace0bf1
Daniel Resa Perez <d.resa.perez@gmail.com>
2024-12-22 23:15:05 +0100

    userId añadido a la clase UserAccount

--- data/UserAccount.java
+++ data/UserAccount.java
@@ -6,12 +6,16 @@ import java.util.Objects;
  * Classe que representa un compte d'usuari.
  */
 public class UserAccount {
+    private final String userId;
     private final String username;
     private final String email;
     private final String password;
     private int monedero;
 
-    public UserAccount(String username, String email, String password, int monedero) {
+    public UserAccount(String userId, String username, String email, String password, int monedero) {
+        if (userId == null || userId.isEmpty()) {
+            throw new IllegalArgumentException("L'identificador d'usuari no pot ser null o buit.");
+        }
         if (username == null || username.isEmpty()) {
             throw new IllegalArgumentException("El nom d'usuari no pot ser null o buit.");
         }
@@ -21,6 +25,7 @@ public class UserAccount {
         if (password == null || password.length() < 6) {
             throw new IllegalArgumentException("La contrasenya ha de tenir almenys 6 caràcters.");
         }
+        this.userId = userId;
         this.username = username;
         this.email = email;
         this.password = password;
@@ -28,6 +33,10 @@ public class UserAccount {
     }
 
     // Getters
+    public String getUserId() {
+        return userId;
+    }
+
     public String getUsername() {
         return username;
     }
@@ -58,19 +67,21 @@ public class UserAccount {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserAccount that = (UserAccount) o;
-        return username.equals(that.username) &&
+        return userId.equals(that.userId) &&
+                username.equals(that.username) &&
                 email.equals(that.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(username, email);
+        return Objects.hash(userId, username, email);
     }
 
     @Override
     public String toString() {
         return "data.data.UserAccount{" +
-                "username='" + username + '\'' +
+                "userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
                 ", email='" + email + '\'' +
                 '}';
     }

33e41df3d69585c1d57ba1ad7b986df71131367a
Aticor Rahman Nopur <145040234+ByloSynGH@users.noreply.github.com>
2024-12-28 18:49:09 +0100

    Add files via upload

---
+++ micromobility/JourneyServiceInterface.java
@@ -0,0 +1,33 @@
+package data.services;
+
+import data.data.GeographicPointInterface;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface JourneyServiceInterface{
+
+        public void JourneyService();
+
+        public LocalDateTime getInitDate();
+
+        public float getDuration();
+
+        public float getDistance();
+
+        public float getAvgSpeed();
+
+        public GeographicPointInterface getOriginPoint();
+
+        public GeographicPointInterface getEndPoint();
+
+        public LocalDateTime getEndDate();
+
+        public BigDecimal getImportCost();
+
+        public boolean isInProgress();
+
+        public void setServiceInit(LocalDateTime initDate,GeographicPointInterface Origin);
+
+        public void setServiceFinish(LocalDateTime endDate, GeographicPointInterface End, BigDecimal imp, float avSP, float dist, float duration);
+}
